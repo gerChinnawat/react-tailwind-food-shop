daf11089eadc63b9896bbf94fc25842020b19b67
chinnawat paksupho <chinnawatpaksupho@gmail.com>
2025-07-18 23:19:25 +0700

    change base path and delete img food folder

--- src/components/cardMenu.tsx
+++ src/components/cardMenu.tsx
@@ -5,7 +5,7 @@ interface CardMenuProps {
 const CardMenu = ({className = ""}: CardMenuProps) => {
     return(
         <article className={className + "flex-shrink-0 cursor-pointer snap-center"}>
-            <img src="/assets/food/food1.jpg" alt="Food1" width="250px" className="rounded-2xl" />
+            <img src="/assets/food.jpg" alt="Food1" width="250px" className="rounded-2xl" />
         </article>
     );
 };
--- vite.config.ts
+++ vite.config.ts
@@ -8,5 +8,5 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
-  base: "/react-tailwind-food-shop"
+  base: "/"
 })
